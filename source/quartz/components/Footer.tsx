32c062933fd9f2553dc02c41061b186c51edcf6f
citsamarth <jts.samarth@cit.ac.in>
2025-10-14 15:35:59 +0530

    Update Footer.tsx

--- source/quartz/components/Footer.tsx
+++ source/quartz/components/Footer.tsx
@@ -15,7 +15,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://citsamarth.github.io/docs">citsamarth.github.io v{version}</a> © {year}
+          <a href="https://citsamarth.github.io/docs">Github Pages</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
