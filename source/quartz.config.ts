71d1e3fb4bcdbb390f4cbc37a577ea32611e3ab9
citsamarth <jts.samarth@cit.ac.in>
2025-10-16 16:29:26 +0530

    vault backup: 2025-10-16 16:29:25

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "CITSamarth Guide",
+    pageTitle: "handbook",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
